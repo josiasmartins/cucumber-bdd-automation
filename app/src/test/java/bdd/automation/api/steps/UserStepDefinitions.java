4a4245ef58b38c8ba6063510e68f50ce156889d5
josias.m.caitano <josiasmartinscaitano@gmail.com>
2023-10-15 10:55:34 -0300

    fix: fixed user step scenary

--- app/src/test/java/bdd/automation/api/steps/UserStepDefinitions.java
+++ app/src/test/java/bdd/automation/api/steps/UserStepDefinitions.java
@@ -1,49 +1,49 @@
 package bdd.automation.api.steps;
 
-import io.cucumber.java.fr.Quand;
-import io.cucumber.java.it.Quando;
-import io.cucumber.java.pt.Entao;
-import io.cucumber.junit.CucumberOptions;
-import io.restassured.http.ContentType;
-
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.java.pt.Entao;
+//import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
-import io.cucumber.java.en.Then;
-import org.apache.http.HttpStatus;
-import org.hamcrest.CoreMatchers;
+import static org.hamcrest.CoreMatchers.is;
 
-//@CucumberOptions(publish = true)
+
 public class UserStepDefinitions {
 
     private Map<String, String> expectedUser = new HashMap<>();
 
-    @Quando("faço um POST para {word} com os seguintes valores:")
-    public void euFacoUmPOSTParaUserComOsSeguintesValores(String endpoint, Map<String, String> user) {
+    @When("I do a POST to {word} with the following values:")
+    @Quando("eu faço um POST para {word} com os seguintes valores:")
+    public void iDoAPOSTToVUserWithTheFollowingValues(String endpoint, Map<String, String> user) {
         expectedUser = user;
 
-        given()
-                .contentType(ContentType.JSON)
-                .body(user);
-        when()
-                .post("http://localhost:12345/api/v3/user" + endpoint)
-        .then()
-            .contentType(ContentType.JSON)
-            .statusCode(HttpStatus.SC_OK);
-
+        given().
+                contentType(ContentType.JSON).
+                body(user).
+                when().
+                post("http://localhost:12345/api" + endpoint).
+                then().
+                contentType(ContentType.JSON).
+                statusCode(HttpStatus.SC_OK);
     }
 
-    @Entao("quando faço um GET para {word}, o usuario criado é retornado")
-    public void quandoFacoUmGETParaVUserRafaelOUsuarioCriadoERetornado(String endpoint) {
-        when()
-                .get("http://localhost:12345/api" + endpoint).
-        then()
-                .contentType(ContentType.JSON)
-                .statusCode(HttpStatus.SC_OK)
-                .body("username", CoreMatchers.is(expectedUser.get("username")));
+    @Then("I receive the created user when I do a GET to {word}")
+    @Entao("quando faço um GET para {word}, o usuário criado é retornado")
+    public void iReceiveTheCreatedUserWhenIDoAGETToVUserRafael(String endpoint) {
+        when().
+                get("http://localhost:12345/api" + endpoint).
+                then().
+                contentType(ContentType.JSON).
+                statusCode(HttpStatus.SC_OK).
+                body("username", is( expectedUser.get("username")));
     }
 }
--- app/src/test/java/resources/features/usuario.feature
+++ app/src/test/java/resources/features/usuario.feature
@@ -7,16 +7,28 @@ Funcionalidade: Gerenciamento de um usuario na Petstore
   Qualquer coisa que faça sentido pro negócio
 
   Cenario: Cria um usuario na loja
-    Quando faço um POST para /v3/user com os seguintes valores:
-      | id         | 10               |
-      | username   | rafael           |
-      | firstName  | Rafael           |
-      | lastName   | Lima             |
-      | email      | rafael@gmail.com |
-      | password   | 12345            |
-      | phone      | 12345            |
-      | userStatus | 1                |
-    Entao quando faço um GET para /v3/user/rafael, o usuario criado é retornado
+    Quando eu faço um POST para /v3/user com os seguintes valores:
+      | id         | 10              |
+      | username   | rafael          |
+      | firstName  | Rafael          |
+      | lastName   | Lima            |
+      | email      | rafael@email.com |
+      | password   | 12345           |
+      | phone      | 12345           |
+      | userStatus | 1               |
+    Então quando faço um GET para /v3/user/rafael, o usuário criado é retornado
+
+#  Cenario: Cria um usuario na loja
+#    Quando eu faço um POST para /v3/user com os seguintes valores:
+#      | id         | 10               |
+#      | username   | rafael           |
+#      | firstName  | Rafael           |
+#      | lastName   | Lima             |
+#      | email      | rafael@gmail.com |
+#      | password   | 12345            |
+#      | phone      | 12345            |
+#      | userStatus | 1                |
+#    Entao quando faço um GET para /v3/user/rafael, o usuario criado é retornado
 
 #  Cenario: Cria um usuario na loja usando docstring
 #    Quando faço um POST para /v3/user com os seguintes docstring:
