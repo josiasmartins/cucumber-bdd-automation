b73966854ffa6db7c933ec1a605dc0e99da4a438
josias.m.caitano <josiasmartinscaitano@gmail.com>
2023-10-17 19:11:50 -0300

    test: assertivas com colecoes groovy

--- app/src/test/java/bdd/automation/api/steps/PetStepDefinitions.java
+++ app/src/test/java/bdd/automation/api/steps/PetStepDefinitions.java
@@ -2,16 +2,20 @@ package bdd.automation.api.steps;
 
 import bdd.automation.api.support.api.PetApi;
 import bdd.automation.api.support.domain.Pet;
-import io.cucumber.java.pt.Dado;
-import io.cucumber.java.pt.E;
-import io.cucumber.java.pt.Entao;
-import io.cucumber.java.pt.Quando;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.java.pt.*;
 import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
-import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 
 import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.is;
 
 public class PetStepDefinitions {
 
@@ -24,71 +28,67 @@ public class PetStepDefinitions {
     }
 
     @Dado("que eu possua animais {word}")
-    public void queEuPossuaAnimaisAvailable(String status) {
-    }
-
+    public void thatIHavePetsAvailable(String status) {}
 
     @Quando("eu pesquiso por todos os animais {word}")
-    public void euPesquisoPorTodosOsAnimaisAvailable(String status) {
+    public void iSearchForAllPetsAvailable(String status) {
         actualPets = petApi.getPetsByStatus(status);
     }
 
-
     @Entao("eu recebo a lista de animais available")
-    public void euReceboAListaDeAnimaisAvailable() {
-        MatcherAssert.assertThat(actualPets, Matchers.is(Matchers.not(Matchers.empty())));
+    public void iReceiveAListOfPetsAvailable() {
+        assertThat(actualPets, Matchers.is(Matchers.not(Matchers.empty())));
     }
 
     @E("eu recebo uma outra lista de animais {word}")
-    public void euReceboUmaOutraListaDeAnimaisAvailable(String status) {
+    public void iReceiveAnotherListOfPetsAvailable(String status) {
         Response actualPetsResponse = petApi.getPetsResponseByStatus(status);
 
         actualPets = actualPetsResponse.body().jsonPath().getList("", Pet.class);
 
-        actualPetsResponse
-                .then()
-                    .statusCode(HttpStatus.SC_OK).
+        actualPetsResponse.
+                then().
+                statusCode(HttpStatus.SC_OK).
                 body(
-                    "size()", Matchers.is(actualPets.size()),
-                        // findAll { it.status == 'available' }.size() faz iteracao e verifica se o status é igual a 'status' e retorne o tamanho
-                        "findAll { it.status == 'available' }.size()", Matchers.is(actualPets.size())
+                        "size()", Matchers.is(actualPets.size()),
+                        "findAll { it.status == '"+status+"' }.size()", Matchers.is(actualPets.size())
                 );
+
     }
 
-    @Entao("eu recebo a lista com {} animal/animais")
-    public void euReceboAListaComAnimais(int petsQuantity) {
-        MatcherAssert.assertThat(actualPets.size(), Matchers.is(petsQuantity));
+    @Então("eu recebo a lista com {} animal/animais")
+    public void iReceiveAListOfPets(int petsQuantity) {
+        assertThat(actualPets.size(), Matchers.is(petsQuantity));
     }
 
     @Dado("que eu não possua animais {word}")
-    public void queEuNãoPossuaAnimaisSold(String status) {
+    public void thatIdontHavePets(String status) {
         petApi.deletePetsByStatus(status);
     }
 
     @Quando("pesquiso por todos os animais {word}")
-    public void pesquisoPorTodosOsAnimaisAvailable(String status) {
+    public void iDoASearchForAllPetsAvailable(String status) {
         actualPetsResponse = petApi.getPetsResponseByStatus(status);
     }
 
-    @Entao("recebo a lista  com {int} animais {word}")
-    public void receboAListaComAnimaisAvailable(int petsQuantity, String status) {
-        actualPetsResponse
-                .then()
-                .statusCode(HttpStatus.SC_OK)
-                .body(
+    @Entao("recebo a lista com {int} animais {word}")
+    public void iReceiveAListOfPetsAvailable(int petsQuantity, String status) {
+        actualPetsResponse.
+                then().
+                statusCode(HttpStatus.SC_OK).
+                body(
                         "size()", Matchers.is(petsQuantity),
-                        "findAll { it.status == '" + status + "' }.size()"
+                        "findAll { it.status == '" + status + "' }.size()", is(petsQuantity)
                 );
+
     }
 
     @E("{int} animais possuem o nome {word}")
-    public void animaisPossuemONomeLion(int petsQuantity, String petsName) {
-        actualPetsResponse
-                .then()
-                .statusCode(HttpStatus.SC_OK)
-                .body(
-                    "findAll { it.name.contains('" + petsName + "') }.size()", Matchers.is(petsQuantity)
+    public void petsHasTheNameLion(int petsQuantity, String petName) {
+        actualPetsResponse.
+                then().
+                body(
+                        "findAll { it.name.contains('"+petName+"') }.size()", Matchers.is(petsQuantity)
                 );
-
     }
 }
--- app/src/test/java/resources/features/animal.feature
+++ app/src/test/java/resources/features/animal.feature
@@ -1,23 +1,22 @@
 # language: pt
-
-Funcionalidade: Gerenciamento de um animal da Loja
+Funcionalidade: Gerenciamento de um animal da PetSore
 
   Cenario: Lista somente animais disponíveis para a venda
     Dado que eu possua animais available
     Quando eu pesquiso por todos os animais available
     Então eu recebo a lista de animais available
-    # Passo desnecessário, somente para exemplo
+#   Passo duplicado, somente para exemplo
     E eu recebo uma outra lista de animais available
 
   Cenario: Lista somente animais pending
-    Dado que eu possua animais peding
+    Dado que eu possua animais pending
     Quando eu pesquiso por todos os animais pending
-    Entao eu recebo a lista com 2 animais
+    Então eu recebo a lista com 2 animais
 
-  Cenario: Não lista nehum animal
+  Cenario: Não lista nenhum animal
     Dado que eu não possua animais sold
     Quando eu pesquiso por todos os animais sold
-    Entao eu recebo a lista com 0 animal
+    Então eu recebo a lista com 0 animal
 
   Esquema do Cenário: Lista animais pelo seu estado de venda
     Dado que eu não possua animais sold
@@ -33,8 +32,8 @@ Funcionalidade: Gerenciamento de um animal da Loja
       | estado | quantidade |
       | sold   | 0          |
 
-  Cenario: Lista de animais disponíveis para a venda
+  Cenario: Lista animais disponíveis para a venda
     Dado que eu possua animais available
     Quando pesquiso por todos os animais available
-    Entao recebo a lista  com 7 animais available
+    Então recebo a lista com 7 animais available
     E 3 animais possuem o nome Lion
